b19bf49ca86d6fa4743180f62db8ae48e3b36d10
eidheim <eidheim@gmail.com>
2018-12-05 13:38:18 +0100

    Corrected path tests. Sometimes /bin is linked to for instance /usr/bin

--- tests/io_test.cpp
+++ tests/io_test.cpp
@@ -37,20 +37,20 @@ int main() {
 
 #ifndef _WIN32
   {
-    Process process("pwd", "/bin", [output](const char *bytes, size_t n) {
+    Process process("pwd", "/usr", [output](const char *bytes, size_t n) {
       *output += string(bytes, n);
     });
     assert(process.get_exit_status() == 0);
-    assert(output->substr(0, 4) == "/bin");
+    assert(output->substr(0, 4) == "/usr");
     output->clear();
   }
 
   {
-    Process process(std::vector<string>{"/bin/pwd"}, "/bin", [output](const char *bytes, size_t n) {
+    Process process(std::vector<string>{"/bin/pwd"}, "/usr", [output](const char *bytes, size_t n) {
       *output += string(bytes, n);
     });
     assert(process.get_exit_status() == 0);
-    assert(output->substr(0, 4) == "/bin");
+    assert(output->substr(0, 4) == "/usr");
     output->clear();
   }
 
