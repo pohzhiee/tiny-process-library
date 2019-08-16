bf455272094a7b51b2f677ff248c4c1862e4bb66
eidheim <eidheim@gmail.com>
2019-08-16 20:09:43 +0200

    Config::inherit_file_descriptors is now supported on Windows

--- process.hpp
+++ process.hpp
@@ -16,7 +16,8 @@ namespace TinyProcessLib {
 struct Config {
   /// Buffer size for reading stdout and stderr. Default is 131072 (128 kB).
   std::size_t buffer_size = 131072;
-  /// Set to true to inherit file descriptors from parent process. Default is false. Only supported on Unix-like systems.
+  /// Set to true to inherit file descriptors from parent process. Default is false.
+  /// On Windows: has no effect unless read_stdout==nullptr, read_stderr==nullptr and open_stdin==false.
   bool inherit_file_descriptors = false;
 };
 
--- process_win.cpp
+++ process_win.cpp
@@ -129,8 +129,9 @@ Process::id_type Process::open(const string_type &command, const string_type &pa
     environment_str += '\0';
 #endif
   }
-  BOOL bSuccess = CreateProcess(nullptr, process_command.empty() ? nullptr : &process_command[0], nullptr, nullptr, true, CREATE_NO_WINDOW,
-                                environment_str.empty() ? nullptr : &environment_str[0], path.empty() ? nullptr : path.c_str(), &startup_info, &process_info);
+  BOOL bSuccess = CreateProcess(nullptr, process_command.empty() ? nullptr : &process_command[0], nullptr, nullptr,
+                                stdin_fd || stdout_fd || stderr_fd || config.inherit_file_descriptors,
+                                CREATE_NO_WINDOW, environment_str.empty() ? nullptr : &environment_str[0], path.empty() ? nullptr : path.c_str(), &startup_info, &process_info);
 
   if(!bSuccess)
     return 0;
